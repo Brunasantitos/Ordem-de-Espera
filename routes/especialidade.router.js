046f654f47c83aeb15858ce6142c28d6439b5308
Brunasantitos <santitos@outlook.com.br>
2023-11-22 17:13:52 -0300

    correção de variaveis

--- routes/especialidade.router.js
+++ routes/especialidade.router.js
@@ -1,13 +1,13 @@
 import express from "express";
 import {
-    especialidadeList,
-    createEspecialidade,
-    deleteEspecialidade
+    listarEspecialidade,
+    criarEspecialidade,
+    deletarEspecialidade
 } from "../controllers/especialidade.controller.js";
 
 export const especialidadeRouter = express.Router();
 
-especialidadeRouter.get("/list", especialidadeList);
-especialidadeRouter.delete('/delete/:id', deleteEspecialidade);
-especialidadeRouter.post('/create', createEspecialidade);
+especialidadeRouter.get("/list", listarEspecialidade);
+especialidadeRouter.delete('/delete/:id', deletarEspecialidade);
+especialidadeRouter.post('/create', criarEspecialidade);
 
